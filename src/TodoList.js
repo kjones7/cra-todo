de88070591e7608ba4f69146fc62a5569f1d2ca2
Kyle Jones <mynameiskyle242@gmail.com>
2023-03-29 23:46:52 -0400

    Implement deleting items

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,30 +2,32 @@ import React, { useState } from 'react';
 
 function TodoList() {
   const [items, setItems] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-
-  function handleInputChange(event) {
-    setInputValue(event.target.value);
-  }
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    if (inputValue.trim() !== '') {
-      setItems([...items, inputValue]);
-      setInputValue('');
+    const newItem = event.target.elements.iteminput.value.trim();
+    if (newItem !== '') {
+      setItems([...items, newItem]);
+      event.target.reset();
     }
+  }
+
+  function handleItemDelete(index) {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
   }
 
   return (
       <div>
         <h1>Todo List</h1>
         <form onSubmit={handleFormSubmit}>
-          <input type="text" value={inputValue} onChange={handleInputChange} />
+          <input type="text" name="iteminput" />
           <button type="submit">Add</button>
         </form>
         <ul>
           {items.map((item, index) => (
-              <li key={index}>{item}</li>
+              <li key={index}>{item} <button type="button" onClick={() => handleItemDelete(index)}>Delete</button></li>
           ))}
         </ul>
       </div>
