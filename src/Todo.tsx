517269299932ab718848449aecf03efe846694c3
Kyle Jones <mynameiskyle242@gmail.com>
2023-04-09 23:33:18 -0400

    Refactor tasks state to be an object with an `isCompleted` key

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -2,9 +2,14 @@ import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import {Button, InputGroup} from "react-bootstrap";
 
+interface Task {
+    task: string;
+    isCompleted: boolean;
+}
+
 function Todo() {
     const [addTaskInputValue, setAddTaskInputValue] = useState<string>('');
-    const [tasks, setTasks] = useState<string[]>([]);
+    const [tasks, setTasks] = useState<Task[]>([]);
 
     // Handlers
     const handleAddTaskInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -12,7 +17,11 @@ function Todo() {
     }
     const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setTasks([...tasks, addTaskInputValue]);
+        const newTask = {
+            task: addTaskInputValue,
+            isCompleted: false,
+        };
+        setTasks([...tasks, newTask]);
         setAddTaskInputValue('');
     };
     const handleDeleteTask = (index: number) => {
@@ -20,9 +29,12 @@ function Todo() {
         setTasks(newTasks);
     }
     const handleTaskChange = (index: number, e: React.ChangeEvent<HTMLInputElement>) => {
-        const newTask = e.target.value;
-        const newTasks = [...tasks];
-        newTasks[index] = newTask;
+        const oldTask = tasks[index];
+        const newTask = {
+            task: e.target.value,
+            isCompleted: oldTask.isCompleted,
+        };
+        var newTasks = tasks.map((task, i) => {return i === index ? newTask : task});
         setTasks(newTasks);
     };
 
@@ -44,7 +56,7 @@ function Todo() {
                             return (
                                 <InputGroup className="mb-3" key={index}>
                                     <InputGroup.Checkbox className="mt-0" />
-                                    <Form.Control type="text" value={task} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleTaskChange(index, e)}/>
+                                    <Form.Control type="text" value={task.task} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleTaskChange(index, e)}/>
                                     <Button
                                         variant="outline-secondary"
                                         onClick={() => handleDeleteTask(index)}
@@ -55,6 +67,9 @@ function Todo() {
                             );
                         })
                     }
+                </Form.Group>
+                <Form.Group>
+                    <h6>Completed</h6>
                 </Form.Group>
             </Form>
         </div>
